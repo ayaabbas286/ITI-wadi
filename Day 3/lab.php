3deba21b97261e9f62e105642be99aa69b83359e
ayaabbas286 <ayaabbas286@gmail.com>
2023-12-11 15:04:02 +0200

    lab 3

--- Day 3/lab.php
+++ Day 3/lab.php
@@ -1,1 +1,26 @@
 <?php
+//1- Create an array that contain values (PHP, HTML, CSS, JavaScript)
+$values=["PHP","HTML","CSS","JavaScript"];
+//2- Print the array elements using loop (2 ways)
+echo"<h5>First Method<h5>";
+for($i=0;$i<=3;$i++)
+{
+    echo $values[$i]."<br>";
+}
+echo "<hr>";
+echo"<h5>Second Method<h5>";
+foreach($values as $lang){
+    echo $lang."<br>";
+}
+echo "<hr>";
+$info=["Name"=>"Aya_Abbas","Age"=>"25","Email"=>"ayaabbas626@gmail.com","Collage"=>"Faculty of Law"];
+echo "<table border='1' width='50%'>";
+foreach ($info as $key => $value) {
+    echo "<tr>
+            <td>$key</td>
+            <td>$value</td>
+          </tr>";
+}
+echo "</table>";
+echo "<hr>";
+?>
